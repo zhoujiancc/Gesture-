6489475bb19e160337b0699c4ea739254dde52ed
user <user@Personal-Mac.local>
2011-05-29 08:39:06 +0800

    Initial Commit

---
+++ Gesture/MainAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MainAppDelegate.h
+//  Gesture
+//
+//  Created by user on 29/5/11.
+//  Copyright (c) 2011 neworigin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Gesture/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Gesture
+//
+//  Created by user on 29/5/11.
+//  Copyright (c) 2011 neworigin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MainAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MainAppDelegate class]));
+    }
+}
